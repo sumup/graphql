ddafa4e6574efd1cf7f4135044d7457e5a0e5236
Gancarlo Jung <dev@addicted.coffee>
2022-06-27 18:53:30 -0300

    Improves test coverage

--- http/operation_test.go
+++ http/operation_test.go
@@ -13,26 +13,69 @@ import (
 )
 
 func Test_SetGraphqlOperation(t *testing.T) {
-	t.Run("SetGraphqlOperation success", func(t *testing.T) {
+	t.Run("SetGraphqlOperation with query", func(t *testing.T) {
 		handlerFunc := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			assert.Equal(t, r.URL.RawQuery, "operation=FooBar")
 
 			_, _ = io.WriteString(w, "{}")
 		})
-		srv := httptest.NewServer(handlerFunc)
-		defer srv.Close()
 
-		transport := http.DefaultTransport
-		transport = SetGraphqlOperation(transport)
-		httpClient := &http.Client{Transport: transport}
+		runner := func(t *testing.T, client *graphql.Client) {
+			ctx := context.Background()
+			req := graphql.NewRequest("query FooBar {}")
+			var resp struct{}
 
-		client := graphql.NewClient(srv.URL, graphql.WithHTTPClient(httpClient))
+			err := client.Run(ctx, req, &resp)
+			assert.NoError(t, err)
+		}
 
-		ctx := context.Background()
-		req := graphql.NewRequest("query FooBar {}")
-		var resp struct{}
-
-		err := client.Run(ctx, req, &resp)
-		assert.NoError(t, err)
+		runTest(t, handlerFunc, runner)
 	})
+
+	t.Run("SetGraphqlOperation with mutation", func(t *testing.T) {
+		handlerFunc := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(t, r.URL.RawQuery, "operation=FooBar")
+
+			_, _ = io.WriteString(w, `{
+				"data": {
+		        "fooBar": {
+		            "messages": [],
+		            "result": null,
+		            "successful": false
+		        }
+		    }
+			}`)
+		})
+
+		runner := func(t *testing.T, client *graphql.Client) {
+			ctx := context.Background()
+			req := graphql.NewMutation(`
+				mutation FooBar {
+					fooBar {
+				    successful
+				    messages
+				    result
+					}
+				}
+			`)
+			var resp struct{}
+
+			err := client.Run(ctx, req, &resp)
+			assert.NoError(t, err)
+		}
+
+		runTest(t, handlerFunc, runner)
+	})
+
+func runTest(t *testing.T, handlerFunc http.HandlerFunc, runner func(*testing.T, *graphql.Client)) {
+	srv := httptest.NewServer(handlerFunc)
+	defer srv.Close()
+
+	transport := http.DefaultTransport
+	transport = SetGraphqlOperation(transport)
+	httpClient := &http.Client{Transport: transport}
+
+	client := graphql.NewClient(srv.URL, graphql.WithHTTPClient(httpClient))
+
+	runner(t, client)
 }
