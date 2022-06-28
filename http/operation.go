3e3b0be8b1370d363598521dea76a5e12d5e954f
Giancarlo Jung <dev@addicted.coffee>
2022-06-28 09:35:16 -0300

    Implements Operation RoundTripper (#9)
    * Adds stretchr/testify
    
    * Implements operation roundtrip
    
    * Fixes go.mod
    
    * Improves http/operation_test.go
    
    * Improves test coverage
    
    * Improves handling when operation is not provided
    
    * Calls Close on copyBody after read

---
+++ http/operation.go
@@ -0,0 +1,54 @@
+package http
+
+import (
+	"io"
+	"net/http"
+	"regexp"
+)
+
+type addOperation struct {
+	inner http.RoundTripper
+}
+
+const operationRegex = `((?:query\s)|(?:mutation\s))(\w+)`
+
+func SetGraphqlOperation(inner http.RoundTripper) http.RoundTripper {
+	return &addOperation{
+		inner: inner,
+	}
+}
+
+func (ug *addOperation) RoundTrip(r *http.Request) (*http.Response, error) {
+	values := r.URL.Query()
+	operation := getOperationName(r)
+
+	if operation != "" {
+		values.Add("operation", operation)
+		r.URL.RawQuery = values.Encode()
+	}
+
+	return ug.inner.RoundTrip(r)
+}
+
+func getOperationName(r *http.Request) string {
+	regex := regexp.MustCompile(operationRegex)
+	getBody := r.GetBody
+
+	copyBody, err := getBody()
+	if err != nil {
+		return ""
+	}
+
+	b, err := io.ReadAll(copyBody)
+	if err != nil {
+		return ""
+	}
+	copyBody.Close()
+
+	operation := regex.FindAllSubmatch(b, -1)
+	if operation != nil {
+		return string(operation[0][2])
+	}
+
+	return ""
+}
---
+++ http/operation_test.go
@@ -0,0 +1,101 @@
+package http
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	graphql "github.com/sumup/graphql"
+)
+
+func Test_SetGraphqlOperation(t *testing.T) {
+	t.Run("SetGraphqlOperation with query", func(t *testing.T) {
+		handlerFunc := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(t, r.URL.RawQuery, "operation=FooBar")
+
+			_, _ = io.WriteString(w, "{}")
+		})
+
+		runner := func(t *testing.T, client *graphql.Client) {
+			ctx := context.Background()
+			req := graphql.NewRequest("query FooBar {}")
+			var resp struct{}
+
+			err := client.Run(ctx, req, &resp)
+			assert.NoError(t, err)
+		}
+
+		runTest(t, handlerFunc, runner)
+	})
+
+	t.Run("SetGraphqlOperation with mutation", func(t *testing.T) {
+		handlerFunc := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(t, r.URL.RawQuery, "operation=FooBar")
+
+			_, _ = io.WriteString(w, `{
+				"data": {
+		        "fooBar": {
+		            "messages": [],
+		            "result": null,
+		            "successful": false
+		        }
+		    }
+			}`)
+		})
+
+		runner := func(t *testing.T, client *graphql.Client) {
+			ctx := context.Background()
+			req := graphql.NewMutation(`
+				mutation FooBar {
+					fooBar {
+				    successful
+				    messages
+				    result
+					}
+				}
+			`)
+			var resp struct{}
+
+			err := client.Run(ctx, req, &resp)
+			assert.NoError(t, err)
+		}
+
+		runTest(t, handlerFunc, runner)
+	})
+
+	t.Run("SetGraphqlOperation when operation name is not provided", func(t *testing.T) {
+		handlerFunc := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			assert.Empty(t, r.URL.RawQuery)
+
+			_, _ = io.WriteString(w, "{}")
+		})
+
+		runner := func(t *testing.T, client *graphql.Client) {
+			ctx := context.Background()
+			req := graphql.NewRequest("query {}")
+			var resp struct{}
+
+			err := client.Run(ctx, req, &resp)
+			assert.NoError(t, err)
+		}
+
+		runTest(t, handlerFunc, runner)
+	})
+}
+
+func runTest(t *testing.T, handlerFunc http.HandlerFunc, runner func(*testing.T, *graphql.Client)) {
+	srv := httptest.NewServer(handlerFunc)
+	defer srv.Close()
+
+	transport := http.DefaultTransport
+	transport = SetGraphqlOperation(transport)
+	httpClient := &http.Client{Transport: transport}
+
+	client := graphql.NewClient(srv.URL, graphql.WithHTTPClient(httpClient))
+
+	runner(t, client)
+}
